c0de58ef99967e870fb9c33fd24665d275106453
jackpeters <jpeters@potomacschool.org>
2021-10-14 09:36:41 -0400

    humanplayer changes

--- HumanPlayer.py
+++ HumanPlayer.py
@@ -12,29 +12,49 @@ class HumanPlayer(Player):
             colStart = int(input("What column do you want ship " + ship + " to start in? 1-10."))
             rowStart = int(input("What row do you want ship " + ship + " to start in? 1-10."))
 
-
-            if self.gridShips.isSpaceWater(rowStart - 1, colStart - 1):
-                if vOrH == "V" or vOrH == "v":
-                    if rowStart-1 + size <= 9:
-                        self.gridShips.changeCol(colStart-1, ship, rowStart-1, size)
+            if 10 >= colStart >= 1 and 10 >= rowStart >= 1:
+                if self.gridShips.isSpaceWater(rowStart - 1, colStart - 1):
+                    if vOrH == "V" or vOrH == "v":
+                        if rowStart-1 + size <= 9:
+                            self.gridShips.changeCol(colStart-1, ship, rowStart-1, size)
+                        else:
+                            print("Your ship goes out of bounds - please try again")
+                            count+=1
+                    elif vOrH == "H" or vOrH == "h":
+                        if colStart-1 + size <= 9:
+                            self.gridShips.changeRow(rowStart-1, ship, colStart-1, size)
+                        else:
+                            print("Your ship goes out of bounds - please try again")
+                            count+=1
                     else:
-                        print("Your ship goes out of bounds - please try again")
+                        print("Illegal value entered - try again.")
                         count+=1
-                elif vOrH == "H" or vOrH == "h":
-                    if colStart-1 + size <= 9:
-                        self.gridShips.changeRow(rowStart-1, ship, colStart-1, size)
-
+                else:
+                    print("Placement illegal, please try again")
+                    count+=1
             else:
                 print("Placement illegal, please try again")
                 count+=1
             self.printGrids()
             count-=1
 
-    def takeTurn(self):
-        colStart = int(input("What column do you want to take your shot in?"))
-        rowStart = int(input("What row do you want to take your shot in?"))
-        self.gridShots.changeSingleSpace(self, rowStart, colStart, "X")
 
+
+    def takeTurn(self, playerGrid):
+        count = 1
+        while count >0:
+            colStart = int(input("What column do you want to take your shot in? 1-10."))
+            rowStart = int(input("What row do you want to take your shot in? 1-10."))
+            if colStart > 10 or rowStart > 10 or colStart < 1 or rowStart < 1:
+                print("Illegal coordinates - Try again")
+                count+=1
+            elif playerGrid.gridShips.isSpaceWater(rowStart - 1, colStart - 1):
+                self.gridShots.changeSingleSpace(self, rowStart-1, colStart-1, "0")
+                print("Sorry! Your shot missed and landed in the water.")
+            else:
+                self.gridShots.changeSingleSpace(self, rowStart-1, colStart-1, "X")
+                print("You got a hit!")
+        count-=1
         pass
 
     def createShipGrid(self):
