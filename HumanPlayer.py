f00bf68ade6ca78f860d5a4452c370d4989b3699
jackpeters <jpeters@potomacschool.org>
2021-10-14 08:42:07 -0400

    human player v2

--- HumanPlayer.py
+++ HumanPlayer.py
@@ -5,22 +5,12 @@ class HumanPlayer(Player):
     def __init__(self):
         super().__init__()
 
-    def placeShip(self):
-        count = 5
+    def placeShip(self, ship, size):
+        count = 1
         while count>0:
-            ship = str(input("What type of ship would you like to place (Only one per type)? A(5), B(4), C(3), D(2), or S(3)?"))
-            vOrH = str(input("Do you want your ship vertically or horizontally? Enter V or H."))
-            colStart = int(input("What column do you want your ship to start in? 1-10."))
-            rowStart = int(input("What row dop you want your ship to start in? 1-10."))
-            if ship == "A" or ship == "a":
-                size = 5
-            elif ship == "B" or ship == "b":
-                size = 4
-            elif ship == "S" or ship == "C" or ship == "s" or ship == "c":
-                size = 3
-            elif ship == "D" or ship == "d":
-                size = 2
-
+            vOrH = str(input("Do you want ship " + ship + " vertically or horizontally? Enter V or H."))
+            colStart = int(input("What column do you want ship " + ship + " to start in? 1-10."))
+            rowStart = int(input("What row do you want ship " + ship + " to start in? 1-10."))
             if self.isPlacementLegal(self.gridShips.grid, rowStart - 1, colStart - 1):
                 if vOrH == "V" or vOrH == "v":
                     self.gridShips.changeCol(colStart-1, ship, rowStart-1, size)
@@ -29,7 +19,15 @@ class HumanPlayer(Player):
             else:
                 print("Placement illegal, please try again")
                 count+=1
+            self.printGrids()
             count-=1
 
+    def createShipGrid(self):
+        self.placeShip( "A" , 5 )
+        self.placeShip( "B", 4 )
+        self.placeShip( "C", 3 )
+        self.placeShip( "S", 3 )
+        self.placeShip( "D", 2 )
+
 player = HumanPlayer()
-player.placeShip()
+player.createShipGrid()
