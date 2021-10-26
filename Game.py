22f71e54b0c258de8b091fab7f001623d7de7bcd
jackpeters <jpeters@potomacschool.org>
2021-10-26 09:08:31 -0400

    ships change

--- Game.py
+++ Game.py
@@ -1,29 +1,25 @@
 from HumanPlayer import *
 from smartComputerPlayer import *
 
-class Game:
-    def __init__(self):
-        self.p1 = HumanPlayer()
-        self.cpu = smartComputerPlayer()
+p1 = HumanPlayer()
+cpu = smartComputerPlayer()
 
-    def createGrids(self):
-        self.cpu.createShipGrid()
-        self.p1.createShipGrid()
+def createGrids():
+    cpu.createShipGrid()
+    p1.createShipGrid()
 
-    def main(self):
-        self.createGrids()
-        while self.p1.stillHasShips() and self.cpu.stillHasShips():
-            self.p1.takeTurn(self.cpu)
-            self.cpu.takeTurn(self.p1)
-            self.p1.printGrids()
+def main():
+    createGrids()
+    while p1.stillHasShips() and cpu.stillHasShips():
+        p1.takeTurn(cpu)
+        cpu.takeTurn(p1)
+        p1.printGrids()
 
-        if not self.p1.stillHasShips():
-            print("You Lose")
-            pass
-        else:
-            print("You Win")
-            pass
+    if not p1.stillHasShips():
+        print("You Lose")
+        pass
+    else:
+        print("You Win")
+        pass
 
-
-g1 = Game()
-g1.main()
+main()
